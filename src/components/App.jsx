a7a7ffc064ba5ca7539aa9297820ca493c2b35d8
arneto19 <armandopdneto@gmail.com>
2022-08-20 03:14:54 -0300

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+
+  )
+};
+
+export default App;
